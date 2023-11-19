c6a05c4d7c0c3f6553e41be742c99da02f47ac76
DevShashankSharma <119869630+DevShashankSharma@users.noreply.github.com>
2023-11-19 06:52:39 +0530

    Introduction to JavaScript & Installing Node.js

---
+++ video 54/new.js
@@ -0,0 +1,3 @@
+console.log("hello world")
+console.log("Code is running")
+console.log("Code is completed")
---
+++ video 54/script.js
@@ -0,0 +1,17 @@
+alert("hello world")
+
+console.log("hello world")
+console.log("Code is running")
+console.log("Code is completed")
+
+
+let a = prompt("Enter your no.")
+console.log(a)
+
+let istrue = confirm("Are you sure to shut down your pc")
+
+if(istrue){
+    console.log("shutting down")
+}else{
+    console.log("not shut")
+}
