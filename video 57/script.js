ef4388a9786d002d952f501bbda7c665c337965d
DevShashankSharma <119869630+DevShashankSharma@users.noreply.github.com>
2023-11-22 19:38:30 +0530

    JavaScript Loops

---
+++ video 57/script.js
@@ -0,0 +1,40 @@
+console.log("Concept of loops");
+
+let a = 1;
+// console.log(a+1);
+// console.log(a+2);
+// console.log(a+3);   
+// console.log(a+4);
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a+i); 
+// }
+
+let obj = {
+    name : "Shashank",
+    age : 21,
+    city : "Delhi",
+    country : "India",
+    role : "Programmer",
+}   
+
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key , element);
+// }
+
+// for (const iterator of "Shashank") {
+//     console.log(iterator);
+// }
+
+let i=0;
+// while (i<6) {
+//     console.log(i);
+//     i++;
+// }
+
+i=10;
+do {
+    console.log(i);
+    i++;
+} while (i<6);
