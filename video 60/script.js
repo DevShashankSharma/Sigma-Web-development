2c7a80bed8ec651ef106055646845edb2261b63d
DevShashankSharma <119869630+DevShashankSharma@users.noreply.github.com>
2023-11-25 17:24:53 +0530

    JavaScript Strings

---
+++ video 60/script.js
@@ -0,0 +1,33 @@
+// console.log("Strings concept");
+let a = "Shashank";
+// console.log(a);
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+// console.log(a[3]);
+// console.log(a[4]);
+// console.log(a[5]);
+// console.log(a[6]);
+// console.log(a[7]);
+
+// console.log(a[8]);
+
+// console.log(a.length)
+
+let real_name  = "Shashank";
+let friend = "Shubham";
+
+// console.log("His name is "+ real_name + " and his friend's name is " + friend);
+// console.log(`His name is ${real_name} and his friend's name is ${friend}`)
+
+
+let b = "Shivam";
+// console.log(b.toLowerCase());
+// console.log(b.toUpperCase());
+// console.log(b.length);
+// console.log(b.slice(1,4));
+// console.log(b.slice(1));
+
+console.log(b.replace("Sh","77"));  //If there is more than 1 occurance than only first occurrance will be replaced 
+
+console.log(b.concat(a," is a good boy"));
