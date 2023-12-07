1e32a5dcfefa6fe5ebc94a6e08c9553f8737dad9
DevShashankSharma <119869630+DevShashankSharma@users.noreply.github.com>
2023-12-07 16:15:49 +0530

    JavaScript Exercise 11 - Solution

--- video 65/script.js
+++ video 65/script.js
@@ -1,4 +1,4 @@
-let no = prompt("Enter an number : ");
+// let no = prompt("Enter an number : ");
 
 // if (no == 0) {
 //   console.log(1);
@@ -16,14 +16,29 @@ let no = prompt("Enter an number : ");
 //   );
 // }
 
+// using reduce new method
+let a = 2;
 
+function factorial(number) {
+  if(number == 0){
+    return 1;
+  }
+  let arr = Array.from(Array(number + 1).keys());
+  // console.log(arr);
+  // console.log(arr.slice(1));
+  let c = arr.slice(1).reduce((a, b) => {
+    return a * b;
+  });
+  return c;
+}
 
+console.log(factorial(a));
 
 // using for loop
-let fact = 1;
-if (no != 0) {
-  for (let i = 1; i <= no; i++) {
-    fact *= i;
-  }
-}
-console.log(fact);
+// let fact = 1;
+// if (no != 0) {
+//   for (let i = 1; i <= no; i++) {
+//     fact *= i;
+//   }
+// }
+// console.log(fact);
