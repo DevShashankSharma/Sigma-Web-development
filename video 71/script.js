0fd52b7aa2a890b34950e8f4196d0a7891146b88
DevShashankSharma <119869630+DevShashankSharma@users.noreply.github.com>
2023-12-09 21:18:51 +0530

    Inserting and Removing Elements using JavaScript

---
+++ video 71/script.js
@@ -0,0 +1,52 @@
+// console.log(document.querySelector(".container").innerHTML)
+// console.log(document.querySelector(".box").innerHTML)
+// console.log(document.querySelector(".container").outerHTML)
+// console.log(document.querySelector(".box").outerHTML)
+// console.log(document.querySelector(".container").innerText)
+
+
+// console.log(document.querySelector(".container").tagName)
+// console.log(document.querySelector(".container").nodeName)
+
+// console.log(document.querySelector(".container").classList)
+// console.log(document.querySelector(".container").className)
+// console.log(document.querySelector(".container").attributes)
+
+
+// console.log(document.querySelector(".container").textContent)
+// console.log(document.querySelector(".container").hidden)
+// console.log(document.querySelector(".container").hidden = true)
+
+
+// document.querySelector(".box").innerHTML = "Hey I am Shashank"
+
+
+// document.designMode = "on"
+
+
+
+// let div = document.createElement("div")
+// div.innerHTML = "Hey I am created"
+// div.setAttribute("class","created")
+// // document.querySelector(".container").appendChild(div)
+// document.querySelector(".container").before(div)
+
+
+
+let cont = document.querySelector(".container")
+cont.insertAdjacentHTML("beforeend","<b> I am under the water, Please help me here too much raining</b>")
+
+
+
+
+
+console.log(document.querySelector(".container").classList)
+
+console.log(document.querySelector(".container").className)  
+
+document.querySelector(".container").classList.add("new")
+document.querySelector(".container").classList.remove("new")
+
+
+document.querySelector(".container").classList.toggle("new")
+document.querySelector(".container").classList.toggle("new")
