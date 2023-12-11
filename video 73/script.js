b63ff19672c52b39e3bc73a23789f40f39853993
DevShashankSharma <119869630+DevShashankSharma@users.noreply.github.com>
2023-12-11 22:28:40 +0530

    JavaScript Exercise 13 - Dynamic Website Builder

---
+++ video 73/script.js
@@ -0,0 +1,43 @@
+console.log("Hello World");
+
+document.querySelector("#url").addEventListener("keyup", function () {
+  let url = document.querySelector("#url").value;
+  let name = document.querySelector(".img-box");
+  name.style.backgroundImage = `url(${url})`;
+  name.style.backgroundSize = "cover";
+  name.style.backgroundPosition = "center";
+  name.style.backgroundRepeat = "no-repeat";
+});
+
+function createCard(url, timestamp, title, author, published_date, views) {
+  let div = document.createElement("div");
+  div.innerHTML = `<div class="card">
+                            <div class="card-img">
+                                <div class="timestamp">${timestamp}</div>
+                            </div>
+                            <div class="card-content">
+                                <div class="card-title">${title}</div> 
+                                <div class="about">
+                                    <div class="card-author">${author}</div>
+                                    <div class="card-date">${published_date}</div>
+                                    <div class="card-views">${views}</div>
+                                </div>
+                            </div>
+                        </div>`;
+
+  document.querySelector(".container").appendChild(div);
+  let i = document.querySelector(".card-img");
+  i.style.backgroundImage = `url(${url})`;
+  i.style.backgroundSize = "cover";
+  i.style.backgroundPosition = "center";
+  i.style.backgroundRepeat = "no-repeat";
+}
+
+createCard(
+  "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLB0alxLSXCSEPITzWr-XXUiv1oglQ",
+  "30:32",
+  "Welcome to JavaScript Tutorial",
+  "CWH",
+  "30-12-2021",
+  "200k"
+);
