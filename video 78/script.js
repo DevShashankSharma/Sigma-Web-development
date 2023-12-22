1012856dbc4f701817452da76c2de3c359e80a2d
DevShashankSharma <119869630+DevShashankSharma@users.noreply.github.com>
2023-12-22 08:02:32 +0530

    JavaScript Exercise 14 - Solution

--- video 78/script.js
+++ video 78/script.js
@@ -1,23 +1,59 @@
 let title = document.querySelector(".title");
 
-const setData = (text) => {
-    let time = Math.floor(Math.random() * 7) + 1; 
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(text);
-        }, time*1000);
-    })
-} 
+const randomDelay = (data) => {
+  let time = Math.floor(Math.random() * 7) + 3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(data);
+    }, time * 1000);
+  });
+};
 
-const hacking = async() => { 
-    title.innerHTML = await setData("Initializing Hacking");  
-    title.innerHTML = await setData("Reading your Files");
-    title.innerHTML =  await setData("Password files Detected");
-    title.innerHTML =  await setData("Sending all passwords and personal files to server");
-    title.innerHTML =  await setData("Cleaning up");
-    title.innerHTML =  await setData("Hacking Completed");
-}
+const setData = async (data) => {
+  let text = await randomDelay(data);
+  let div = document.createElement("div");
+  div.innerHTML = ">>> " + text; 
+  title.appendChild(div);
+};
+
+const test = () => {
+  let last = document.body.getElementsByTagName("div");
+  last = last[last.length - 2];
+  if (last.innerHTML.endsWith("...")) {
+    last.innerHTML = last.innerHTML.slice(0, -3);
+  }
+  if (last.innerHTML.endsWith("..")) {
+    last.innerHTML = last.innerHTML.slice(0, -2);
+  }
+  if (last.innerHTML.endsWith(".")) {
+    last.innerHTML = last.innerHTML.slice(0, -1);
+  }
+};
+
+let text = [
+  "Initializing Hacking",
+  "Reading your Files",
+  "Password files Detected",
+  "Sending all passwords and personal files to server",
+  "Cleaning up",
+  "Hacking Completed",
+];
+
+const hacking = async () => {
+  let t = setInterval(() => {
+    let last = document.body.getElementsByTagName("div");
+    last = last[last.length - 1];
+    if (last.innerHTML.endsWith("..."))
+      last.innerHTML = last.innerHTML.slice(0, -3);
+    else last.innerHTML = last.innerHTML + ".";
+  }, 300);
+
+  for (let i = 0; i < text.length; i++) {
+    await setData(text[i]);
+    test();
+  }
+
+  clearInterval(t);
+};
 
 hacking();
-
-console.log('Hello World');
