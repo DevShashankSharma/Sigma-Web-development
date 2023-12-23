c7d0fb072facc9e8a095cf0d8912a1a94500ea20
DevShashankSharma <119869630+DevShashankSharma@users.noreply.github.com>
2023-12-23 06:44:43 +0530

    Advanced JavaScript

---
+++ video 82/script.js
@@ -0,0 +1,37 @@
+async function sleep (){
+    return new Promise((resolve,reject) =>{
+        setTimeout(() => {
+            resolve(45)
+        },1000)
+    })
+}
+
+function sum(a,b,c){
+    return a+b+c;
+}
+
+(async function main(){
+    // let a = await sleep()
+    // let b = await sleep()
+    // console.log(a,b)
+
+    // let [x,y] = [1,5,7]
+    // console.log(x,y)
+
+    // let [a,b, ...rest] = [1,2,3,4,5,6,2,8,3]
+    // console.log(a,b,rest)
+
+    // let obj = {
+    //     a : 1,
+    //     b : 2,
+    //     c : 3
+    // }
+
+    // let {a,b} = obj
+    // console.log(a,b)
+
+
+    let arr = [1,2,3] 
+    console.log(sum(arr[0] , arr[1] , arr[2]))
+    console.log(sum(...arr))
+})()
