422acc8bc615d829467b259feced96da1b6d0993
DevShashankSharma <editorshashank429@gmail.com>
2024-01-02 18:10:24 +0530

    CommonJs Vs EcmaScript Modules

---
+++ video 86/mymodule.js
@@ -0,0 +1,12 @@
+// named export
+export const a = 10  
+export const b = 20
+export const c = 30
+export const d = 40
+
+//default export
+let obj
+export default obj = {
+    a: 10,
+    b: 20
+}
---
+++ video 86/mymodule2.js
@@ -0,0 +1,7 @@
+// module.exports = {
+//     a: 10,
+//     b: 20
+// }
+
+let c = 56;
+module.exports = c
