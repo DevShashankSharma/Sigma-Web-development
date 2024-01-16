2fd5cc2967123409858124f685d397a6627ac8f6
DevShashankSharma <editorshashank429@gmail.com>
2024-01-16 17:58:31 +0530

    Exercise 16 solution

--- video 97/main.js
+++ video 97/main.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 import express from "express";
 import { Employee } from "./models/employee.js";
-import cors from 'cors';
+import cors from "cors";
 
 // Connecting with the database
 mongoose.connect("mongodb://localhost:27017/company");
 
 // Creating a port
-const app = express(); 
+const app = express();
 const port = 3000;
 
 // Middleware to parse JSON in request body
@@ -21,28 +21,48 @@ app.post("/", async (req, res) => {
       salary: req.body.salary,
       language: req.body.language,
       city: req.body.city,
-      isManager: req.body.isManager=="yes"?true:false,
+      isManager: req.body.isManager == "yes" ? true : false,
     });
-    console.log(employee);
-
     await employee.save();
-    res.json({ message: 'Employee data saved successfully!' });
+    console.log(employee,"Employee data saved successfully!");
+    res.json({ message: "Employee data saved successfully!" });
   } catch (error) {
-    console.error('Error saving employee data:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error saving employee data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 app.get("/", async (req, res) => {
   try {
     //Delete all data
-    // await Employee.deleteMany({});
+    await Employee.deleteMany({});
     // Retrieve all employee data
     const data = await Employee.find();
     res.json(data);
   } catch (error) {
-    console.error('Error retrieving employee data:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error retrieving employee data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/check", async (req, res) => {
+  try {
+    const data = await Employee.find(req.query);
+    console.log(data)
+    res.json(data);
+  } catch (error) {
+    console.error("Error retrieving employee data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.delete("/remove", async (req, res) => {
+  try {
+    const data = await Employee.deleteOne(req.query);
+    res.json(data);
+  } catch (error) {
+    console.error("Error retrieving employee data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
