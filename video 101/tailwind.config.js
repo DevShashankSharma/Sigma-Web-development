bc56475783c769179f51b5ccdec02b07b4cb417c
DevShashankSharma <editorshashank429@gmail.com>
2024-01-23 09:12:05 +0530

    X.com (Twitter) Clone using Tailwind CSS

---
+++ video 101/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.html'],
+  theme: {
+    extend: {
+      screens: {
+        'xl': '1283px',
+        'ssm': '550px',
+        'sssm': '400px',
+      },
+    },
+  },
+  plugins: [],
+}
+
