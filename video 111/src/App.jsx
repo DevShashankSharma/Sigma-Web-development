dbffb28b1822a4c3cfeb522db00ccc35ddd8aef3
DevShashankSharma <editorshashank429@gmail.com>
2024-02-07 06:44:46 +0530

    Exercise 18 - Display the Cards

---
+++ video 111/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setPosts(json));
+  });
+
+  return (
+    <>
+      <div className="container grid grid-cols-3 gap-4 items-center justify-center">
+        {posts.map((post) => {
+          return (
+            <div className="card flex flex-col gap-2 bg-red-50 h-[100%]">
+              <div className="title font-bold text-2xl">
+                {post.title ? post.title : "No title"}
+              </div>
+              <div className="body font-normal text-lg">
+                {post.body ? post.body : "No body"}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
