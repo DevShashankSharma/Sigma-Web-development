26856b37641d2234abcc2ed567f5ed1c11fd9044
DevShashankSharma <editorshashank429@gmail.com>
2024-02-16 17:43:25 +0530

    The useContext hook in React

---
+++ video 116/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from './Button'
+
+const Navbar = () => {
+    return (
+        <div>
+            <div>
+                i am an Navbar
+            </div>
+            <Button />
+        </div>
+    )
+}
+
+export default Navbar
