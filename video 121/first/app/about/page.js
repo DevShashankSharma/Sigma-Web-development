30b53cf12a5d989f68a9f8e2eb3839e2782ade0c
DevShashankSharma <editorshashank429@gmail.com>
2024-02-22 20:55:01 +0530

    Introduction to Next.js & File based routing

---
+++ video 121/first/app/about/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <h1 className="text-xl">Problem Solved By Next js</h1>
+            <ul>
+                <li>Full Stack Solution</li>
+                <li>File Based Routing</li>
+                <li>Additional Feature like  router from next/navigation</li>
+                <li>Optimized rendering</li>
+            </ul>
+        </div>
+    )
+}
+
+export default page
